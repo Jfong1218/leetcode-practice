cc9f5f572c424dd99f7d076f4182f306ff5b8737
jonathan fong <jonathan1218@gmail.com>
2023-05-17 17:56:22 -0700

    initial commit

---
+++ findLargestDifference.js
@@ -0,0 +1,23 @@
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function largestDiff(arr) {
+    // your code here
+    let difference = 0;
+    if (arr.length <= 1){
+        return difference
+    }
+    for (i = 0; i < arr.length+1; i++){
+        for (j = i+1; j < arr.length+1; j++){
+            if (Math.abs(arr[i]-arr[j]) > difference) {
+                // console.log(arr[i] +" and "+ arr[j]+" diff is "+Math.abs(arr[i]-arr[j]))
+                difference = Math.abs(arr[i]-arr[j]);
+            }
+        }
+    }
+    return difference
+  }
+
+  console.log(largestDiff([-1,2,3,10,9]))
